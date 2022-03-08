61de7d880086690a1508ed7df7b7e04e1ac361a3
Nagy Kristof <nagykristof958@gmail.com>
2022-03-08 17:30:49 +0100

    Added dummy recylerView

---
+++ app/src/main/java/hu/kristof/nagy/hikebookclient/view/mymap/MyMapListAdapter.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2019, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// based on:
+// https://github.com/google-developer-training/android-kotlin-fundamentals-apps/tree/master/RecyclerViewFundamentals
+
+package hu.kristof.nagy.hikebookclient.view.mymap
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hikebookclient.databinding.MyMapListItemBinding
+
+class MyMapListAdapter : RecyclerView.Adapter<MyMapListAdapter.ViewHolder>() {
+
+    private val dummyData = listOf(
+        "Útvonal név 1",
+        "Útvonal név 2",
+        "Útvonal név 3",
+        "Útvonal név 4",
+        "Útvonal név 5"
+    )
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = dummyData[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int = dummyData.size
+
+    class ViewHolder private constructor(val binding: MyMapListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val tv: TextView = binding.myMapListItemTextView
+
+        fun bind(str: String) {
+            tv.text = str
+        }
+
+        companion object {
+            fun from(parent: ViewGroup) : ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = MyMapListItemBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
+    }
+}
--- app/src/main/java/hu/kristof/nagy/hikebookclient/view/mymap/MyMapListFragment.kt
+++ app/src/main/java/hu/kristof/nagy/hikebookclient/view/mymap/MyMapListFragment.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.hikebookclient.R
+import com.example.hikebookclient.databinding.FragmentMyMapListBinding
 
 class MyMapListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_my_map_list, container, false)
+        val binding: FragmentMyMapListBinding =DataBindingUtil.inflate(
+            inflater, R.layout.fragment_my_map_list, container, false
+        )
+
+        binding.myMapRecyclerView.adapter = MyMapListAdapter()
+
+        return binding.root
     }
 }
