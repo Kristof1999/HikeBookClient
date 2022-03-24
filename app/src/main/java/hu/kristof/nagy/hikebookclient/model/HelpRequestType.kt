b8168e396d3015cef091f51333233156e252a935
Nagy Kristof <nagykristof958@gmail.com>
2022-03-24 08:15:27 +0100

    Added more help text and options

--- app/src/main/java/hu/kristof/nagy/hikebookclient/model/HelpRequestType.kt
+++ app/src/main/java/hu/kristof/nagy/hikebookclient/model/HelpRequestType.kt
@@ -1,5 +1,5 @@
 package hu.kristof.nagy.hikebookclient.model
 
 enum class HelpRequestType {
-    MY_MAP
+    MY_MAP, MY_MAP_LIST, MY_MAP_DETAIL, ROUTE_CREATE, ROUTE_EDIT, BROWSE_LIST, BROWSE_DETAIL
 }
