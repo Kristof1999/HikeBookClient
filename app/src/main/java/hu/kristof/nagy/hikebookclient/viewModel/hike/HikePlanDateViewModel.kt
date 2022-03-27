5d7017e821d0cf6fa2fa1331fb176d55c5d64110
Nagy Kristof <nagykristof958@gmail.com>
2022-03-27 10:10:49 +0200

    Handled weather response

--- app/src/main/java/hu/kristof/nagy/hikebookclient/viewModel/hike/HikePlanDateViewModel.kt
+++ app/src/main/java/hu/kristof/nagy/hikebookclient/viewModel/hike/HikePlanDateViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import hu.kristof.nagy.hikebookclient.data.WeatherRepository
 import hu.kristof.nagy.hikebookclient.model.Point
+import hu.kristof.nagy.hikebookclient.model.weather.WeatherResponse
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -21,9 +22,47 @@ class HikePlanDateViewModel @Inject constructor(
 
     fun forecast(points: List<Point>) {
         viewModelScope.launch {
-            _forecastRes.value = repository.forecast(
-                points[0].latitude, points[0].longitude,
-            ).toString()
+            var res = ""
+
+            val responseStart = repository.forecast(
+                points.first().latitude, points.first().longitude,
+            )
+            val idxStart = 0
+            res += "Túra kezdetén:\n" + mapResponse(responseStart, idxStart)
+
+            val pointsMiddle = points.size/2
+            val responseMiddle = repository.forecast(
+                points[pointsMiddle].latitude, points[pointsMiddle].longitude,
+            )
+            val idxMiddle = 1
+            res += "Túra felénél:\n" + mapResponse(responseMiddle, idxMiddle)
+
+            val responseEnd = repository.forecast(
+                points.last().latitude, points.last().longitude,
+            )
+            val idxEnd = 2
+            res += "Túra végénél:\n" + mapResponse(responseEnd, idxEnd)
+
+            _forecastRes.value = res
         }
+    }
+
+    private fun mapResponse(response: WeatherResponse, idx: Int): String {
+        val cityName = response.city?.name
+        val dateTime = response.list?.get(idx)?.dtTxt
+        val temp = response.list?.get(idx)?.main?.feelsLike
+        val description = response.list?.get(idx)?.weather?.get(0)?.description
+        val cloudPrecent = response.list?.get(idx)?.clouds?.all
+        val rainPrecent = response.list?.get(idx)?.pop
+        val rainVolume = response.list?.get(idx)?.rain?.vol
+        val windSpeed = response.list?.get(idx)?.wind?.speed
+        val visibilityMetres = response.list?.get(idx)?.visibility
+        return "Város név: $cityName, dátum és idő: $dateTime\n" +
+                    "Várható hőmérséklet: $temp\n°C" +
+                    "Várható időjárás: $description\n" +
+                    "Felhő valószínűség: $cloudPrecent%\n" +
+                    "Csapadék valószínűség: ${rainPrecent?.times(100)}%\n" +
+                    "Várható csapadék az elmúlt 3 órában: $rainVolume\n" +
+                    "Szélerő: $windSpeed, Láthatóság: ${visibilityMetres}m\n"
     }
 }
