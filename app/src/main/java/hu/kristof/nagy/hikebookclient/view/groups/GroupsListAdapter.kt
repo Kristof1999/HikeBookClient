37a7ad9c03c361d44df1ec928e9551d5ab48f296
Nagy Kristof <nagykristof958@gmail.com>
2022-04-06 08:31:11 +0200

    Refactoring

--- app/src/main/java/hu/kristof/nagy/hikebookclient/view/groups/GroupsListAdapter.kt
+++ app/src/main/java/hu/kristof/nagy/hikebookclient/view/groups/GroupsListAdapter.kt
@@ -3,7 +3,6 @@ package hu.kristof.nagy.hikebookclient.view.groups
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -27,8 +26,7 @@ class GroupsListAdapter(
         private val isConnectedPage: Boolean,
         private val clickListener: GroupsClickListener
     ) : RecyclerView.ViewHolder(binding.root) {
-        val name: TextView = binding.groupsListItemGroupNameButton
-        val button: Button = binding.groupsListItemButton
+        private val button: Button = binding.groupsListItemButton
 
         fun bind(groupName: String) {
             binding.groupName = groupName
