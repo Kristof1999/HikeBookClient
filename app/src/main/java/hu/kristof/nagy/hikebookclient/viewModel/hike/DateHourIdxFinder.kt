004c6a5ebbc36bcdae419ce1cad28644c3cee7c4
Nagy Kristof <nagykristof958@gmail.com>
2022-04-20 15:21:37 +0200

    Refactoring

--- app/src/main/java/hu/kristof/nagy/hikebookclient/viewModel/hike/DateHourIdxFinder.kt
+++ app/src/main/java/hu/kristof/nagy/hikebookclient/viewModel/hike/DateHourIdxFinder.kt
@@ -6,16 +6,17 @@ import hu.kristof.nagy.hikebookclient.model.weather.WeatherResponse
  * Finds the index in the given WeatherResponse for the given hour and date.
  */
 object DateHourIdxFinder {
-    fun findHourIdx(hour: Int, response: WeatherResponse, dayStartIdx: Int): Int {
+    fun findHourIdx(hour: Int, response: WeatherResponse, dateIdx: Int): Int {
         for (i in 0..7) {
-            val hourResponse = response?.list?.get(dayStartIdx + i)?.dtTxt?.substring(11, 13)!!.toInt()
-            val nextHourResponse = response?.list?.get(dayStartIdx + i + 1)?.dtTxt?.substring(11, 13)!!.toInt()
+            val hourResponse = response?.list?.get(dateIdx + i)?.dtTxt?.substring(11, 13)!!.toInt()
+            val nextHourResponse = response?.list?.get(dateIdx + i + 1)?.dtTxt?.substring(11, 13)!!.toInt()
             if (hour in hourResponse..nextHourResponse) {
                 // which hourResponse is hour closer to?
-                if (hour - hourResponse < nextHourResponse - hour)
-                    return dayStartIdx + i
-                else
-                    return dayStartIdx + i + 1
+                return if (hour - hourResponse < nextHourResponse - hour) {
+                    dateIdx + i
+                } else {
+                    dateIdx + i + 1
+                }
             }
         }
         throw IndexOutOfBoundsException("$hour cannot be found in the weather response: $response")
