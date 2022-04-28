f5c0b086aa0ecd8c2191e1bcd33f506a312b354d
Nagy Kristof <nagykristof958@gmail.com>
2022-04-28 15:04:55 +0200

    Improved ui test

--- app/src/androidTest/java/hu/kristof/nagy/hikebookclient/view/routes/authentication/StartFragmentTest.kt
+++ app/src/androidTest/java/hu/kristof/nagy/hikebookclient/view/routes/authentication/StartFragmentTest.kt
@@ -1,24 +1,31 @@
-package hu.kristof.nagy.hikebookclient.viewmodel.routes.authentication
+package hu.kristof.nagy.hikebookclient.view.routes.authentication
 
-import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import hu.kristof.nagy.hikebookclient.R
+import hu.kristof.nagy.hikebookclient.launchFragmentInHiltContainer
 import hu.kristof.nagy.hikebookclient.view.authentication.StartFragment
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @MediumTest
+@HiltAndroidTest
 @RunWith(AndroidJUnit4::class)
 class StartFragmentTest {
 
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
     @Test
     fun checkDisplay() {
-        launchFragmentInContainer<StartFragment>(themeResId = R.style.Theme_HikeBookClient)
+        launchFragmentInHiltContainer<StartFragment>(themeResId = R.style.Theme_HikeBookClient)
 
         onView(withId(R.id.aboutPageButton)).check(matches(isDisplayed()))
     }
