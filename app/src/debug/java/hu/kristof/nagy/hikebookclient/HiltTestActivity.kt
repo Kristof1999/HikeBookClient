b4a35aa0a645813d05b8826a1de59b1621f51050
Nagy Kristof <nagykristof958@gmail.com>
2022-05-02 08:00:28 +0200

    Set main activity content for view for testing

--- app/src/debug/java/hu/kristof/nagy/hikebookclient/HiltTestActivity.kt
+++ app/src/debug/java/hu/kristof/nagy/hikebookclient/HiltTestActivity.kt
@@ -16,8 +16,14 @@
 
 package hu.kristof.nagy.hikebookclient
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HiltTestActivity : AppCompatActivity()
+class HiltTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
