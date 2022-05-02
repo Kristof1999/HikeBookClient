8b5c250fa529da8feec6cc22682c943a8ec77765
Nagy Kristof <nagykristof958@gmail.com>
2022-05-02 09:23:05 +0200

    Improved testing

--- app/src/androidTest/java/hu/kristof/nagy/hikebookclient/view/groups/GroupsDetailMembersFragmentTest.kt
+++ app/src/androidTest/java/hu/kristof/nagy/hikebookclient/view/groups/GroupsDetailMembersFragmentTest.kt
@@ -1,13 +1,29 @@
 package hu.kristof.nagy.hikebookclient.view.groups
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import hu.kristof.nagy.hikebookclient.R
+import hu.kristof.nagy.hikebookclient.data.IGroupsRepository
+import hu.kristof.nagy.hikebookclient.di.GroupsRepositoryModule
 import hu.kristof.nagy.hikebookclient.launchFragmentInHiltContainer
+import hu.kristof.nagy.hikebookclient.model.ServerResponseResult
+import hu.kristof.nagy.hikebookclient.util.DataBindingIdlingResource
+import hu.kristof.nagy.hikebookclient.util.DataBindingIdlingResourceRule
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
 
+@UninstallModules(GroupsRepositoryModule::class)
 @HiltAndroidTest
 @MediumTest
 @RunWith(AndroidJUnit4::class)
@@ -15,8 +31,34 @@ class GroupsDetailMembersFragmentTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
-    fun checkList() {
-        launchFragmentInHiltContainer<GroupsDetailMembersFragment>()
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
 
+    @get:Rule
+    var dataBindingIdlingResourceRule = DataBindingIdlingResourceRule(dataBindingIdlingResource)
+
+    @Mock
+    @BindValue
+    lateinit var groupsRepository: IGroupsRepository
+
+    @Test
+    fun checkList() {
+        val groupName = "group"
+        val user1 = "user1"
+        val user2 = "user2"
+        groupsRepository = mock {
+            onBlocking {
+                listMembers(groupName)
+            } doReturn ServerResponseResult(true, null, listOf(
+                user1, user2
+            ))
+        }
+        val bundle = GroupsDetailMembersFragmentArgs(groupName).toBundle()
+        launchFragmentInHiltContainer<GroupsDetailMembersFragment>(bundle, dataBindingIdlingResource)
+
+        onView(withId(R.id.groupsDetailMembersRecyclerView))
+            .check(matches(isDisplayed()))
+            .check(matches(hasDescendant(withChild(withId(R.id.groupsDetailMemberNameTv)))))
+            .check(matches(hasDescendant(withChild(withText(user1)))))
+            .check(matches(hasDescendant(withChild(withText(user2)))))
     }
 }
