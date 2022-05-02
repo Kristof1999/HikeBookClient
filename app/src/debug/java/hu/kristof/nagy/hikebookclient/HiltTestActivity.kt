60f8275269651906af2422b3733519e46c4d6883
Nagy Kristof <nagykristof958@gmail.com>
2022-05-02 11:54:54 +0200

    Fixed hilt test activity

--- app/src/debug/java/hu/kristof/nagy/hikebookclient/HiltTestActivity.kt
+++ app/src/debug/java/hu/kristof/nagy/hikebookclient/HiltTestActivity.kt
@@ -16,14 +16,8 @@
 
 package hu.kristof.nagy.hikebookclient
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HiltTestActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+class HiltTestActivity : AppCompatActivity()
