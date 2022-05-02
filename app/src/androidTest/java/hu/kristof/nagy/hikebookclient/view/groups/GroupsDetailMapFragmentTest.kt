6e9caaa5bcf0c2f669f982f74d7b397b41033c77
Nagy Kristof <nagykristof958@gmail.com>
2022-05-02 09:28:45 +0200

    Added navigation test

--- app/src/androidTest/java/hu/kristof/nagy/hikebookclient/view/groups/GroupsDetailMapFragmentTest.kt
+++ app/src/androidTest/java/hu/kristof/nagy/hikebookclient/view/groups/GroupsDetailMapFragmentTest.kt
@@ -1,7 +1,10 @@
 package hu.kristof.nagy.hikebookclient.view.groups
 
 import androidx.core.os.bundleOf
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -14,6 +17,7 @@ import hu.kristof.nagy.hikebookclient.R
 import hu.kristof.nagy.hikebookclient.data.routes.IGroupRouteRepository
 import hu.kristof.nagy.hikebookclient.di.GroupRouteRepositoryModule
 import hu.kristof.nagy.hikebookclient.launchFragmentInHiltContainer
+import hu.kristof.nagy.hikebookclient.model.RouteType
 import hu.kristof.nagy.hikebookclient.model.ServerResponseResult
 import hu.kristof.nagy.hikebookclient.util.Constants
 import hu.kristof.nagy.hikebookclient.util.DataBindingIdlingResource
@@ -23,8 +27,10 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
 
 @UninstallModules(GroupRouteRepositoryModule::class)
 @HiltAndroidTest
@@ -86,5 +92,31 @@ class GroupsDetailMapFragmentTest {
 
         onView(withId(R.id.groupsMapCreateRouteFab)).check(matches(not(isDisplayed())))
         onView(withId(R.id.groupsMapAddFromMyMapButton)).check(matches(not(isDisplayed())))
+    }
+
+    @Test
+    fun verifyRouteCreateNav() {
+        val groupName = "group"
+        val isConnectedPage = true
+        val bundle = bundleOf(
+            Constants.GROUP_NAME_BUNDLE_KEY to groupName,
+            Constants.IS_CONNECTED_PAGE_BUNDLE_KEY to isConnectedPage
+        )
+        groupRouteRepository = mock {
+            onBlocking {
+                loadGroupRoutes(groupName)
+            } doReturn ServerResponseResult(true, null, listOf())
+        }
+        val navController = Mockito.mock(NavController::class.java)
+        launchFragmentInHiltContainer<GroupsDetailMapFragment>(bundle, dataBindingIdlingResource) {
+            Navigation.setViewNavController(requireActivity().findViewById(R.id.navHostFragment), navController)
+        }
+
+        onView(withId(R.id.groupsMapCreateRouteFab)).perform(click())
+
+        val routeType = RouteType.GROUP
+        val directions = GroupsDetailFragmentDirections
+            .actionGroupsDetailFragmentToRouteCreateFragment(routeType, groupName)
+        verify(navController).navigate(directions)
     }
 }
