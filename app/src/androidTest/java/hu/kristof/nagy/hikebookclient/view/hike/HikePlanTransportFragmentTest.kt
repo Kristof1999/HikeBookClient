781795c4d00c625bbd41719c7388e2c018c79a1c
Nagy Kristof <nagykristof958@gmail.com>
2022-05-03 18:27:55 +0200

    Added test for switches

--- app/src/androidTest/java/hu/kristof/nagy/hikebookclient/view/hike/HikePlanTransportFragmentTest.kt
+++ app/src/androidTest/java/hu/kristof/nagy/hikebookclient/view/hike/HikePlanTransportFragmentTest.kt
@@ -1,17 +1,33 @@
 package hu.kristof.nagy.hikebookclient.view.hike
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
 import hu.kristof.nagy.hikebookclient.R
+import hu.kristof.nagy.hikebookclient.data.routes.IUserRouteRepository
+import hu.kristof.nagy.hikebookclient.di.UserRouteRepositoryModule
 import hu.kristof.nagy.hikebookclient.launchFragmentInHiltContainer
+import hu.kristof.nagy.hikebookclient.model.Point
+import hu.kristof.nagy.hikebookclient.model.ServerResponseResult
+import hu.kristof.nagy.hikebookclient.model.routes.Route
+import hu.kristof.nagy.hikebookclient.util.DataBindingIdlingResource
+import hu.kristof.nagy.hikebookclient.util.DataBindingIdlingResourceRule
+import hu.kristof.nagy.hikebookclient.view.mymap.MarkerType
+import kotlinx.coroutines.flow.flowOf
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
 
+@UninstallModules(UserRouteRepositoryModule::class)
 @HiltAndroidTest
 @MediumTest
 @RunWith(AndroidJUnit4::class)
@@ -19,8 +35,28 @@ class HikePlanTransportFragmentTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
+
+    @get:Rule
+    var dataBindingIdlingResourceRule = DataBindingIdlingResourceRule(dataBindingIdlingResource)
+
+    private val routeName = "route"
+    private val route = Route.UserRoute("user", routeName, listOf(
+        Point(0.0, 0.0, MarkerType.SET, ""),
+        Point(0.0, 0.0, MarkerType.NEW, "")
+    ), "")
+    @BindValue
+    val repository = mock<IUserRouteRepository> {
+        onBlocking {
+            loadUserRouteOfLoggedInUser(routeName)
+        } doReturn flowOf(ServerResponseResult(true, null, route))
+    }
+
+    @Test
     fun checkDisplay() {
-        launchFragmentInHiltContainer<HikePlanTransportFragment>()
+        val isForward = true
+        val bundle = HikePlanTransportFragmentArgs(isForward, routeName).toBundle()
+        launchFragmentInHiltContainer<HikePlanTransportFragment>(bundle, dataBindingIdlingResource)
 
         onView(withId(R.id.hikePlanTransportStartSwitch))
             .check(matches(isDisplayed()))
@@ -38,8 +74,29 @@ class HikePlanTransportFragmentTest {
             .check(matches(isDisplayed()))
     }
 
+    @Test
     fun checkSwitches() {
+        val isForward = true
+        val bundle = HikePlanTransportFragmentArgs(isForward, routeName).toBundle()
+        launchFragmentInHiltContainer<HikePlanTransportFragment>(bundle, dataBindingIdlingResource)
 
+        onView(withId(R.id.hikePlanTransportStartSwitch)).check(matches(isNotChecked()))
+        onView(withId(R.id.hikePlanTransportEndSwitch)).check(matches(isNotChecked()))
+
+        onView(withId(R.id.hikePlanTransportStartSwitch)).perform(click())
+
+        onView(withId(R.id.hikePlanTransportStartSwitch)).check(matches(isChecked()))
+        onView(withId(R.id.hikePlanTransportEndSwitch)).check(matches(isNotChecked()))
+
+        onView(withId(R.id.hikePlanTransportEndSwitch)).perform(click())
+
+        onView(withId(R.id.hikePlanTransportStartSwitch)).check(matches(isNotChecked()))
+        onView(withId(R.id.hikePlanTransportEndSwitch)).check(matches(isChecked()))
+
+        onView(withId(R.id.hikePlanTransportEndSwitch)).perform(click())
+
+        onView(withId(R.id.hikePlanTransportStartSwitch)).check(matches(isNotChecked()))
+        onView(withId(R.id.hikePlanTransportEndSwitch)).check(matches(isNotChecked()))
     }
 
     fun checkSpinner() {
